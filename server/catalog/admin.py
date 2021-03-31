11e40d90834222cba6cd6216b3faeb1ebf227c5e
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-30 17:38:24 -0700

    fisrt commit

---
+++ server/catalog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Wine
+
+
+@admin.register(Wine)
+class WineAdmin(admin.ModelAdmin):
+    fields = ('id', 'country', 'description', 'points', 'price', 'variety', 'winery',)
+    list_display = ('id', 'country', 'points', 'price', 'variety', 'winery')
+    list_filter = ('variety',)
+    readonly_fields = ('id',)
